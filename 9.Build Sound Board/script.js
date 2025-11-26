1e4bb93615d6948a54a5b32ecf11fdcdc24bca53
bul-bul-sha-rma-14325 <127110802+bul-bul-sha-rma-14325@users.noreply.github.com>
2025-11-26 15:47:24 +0530

    HTML , CSS & JS

---
+++ 9.Build Sound Board/script.js
@@ -0,0 +1,12 @@
+document.getElementById("sound1").addEventListener("click", function () {
+  const audio = new Audio("audio/first.mp3");
+  audio.play();
+});
+document.getElementById("sound2").addEventListener("click", function () {
+  const audio = new Audio("audio/second.mp3.mp3");
+  audio.play();
+});
+document.getElementById("sound3").addEventListener("click", function () {
+  const audio = new Audio("audio/third.mp3");
+  audio.play();
+});
